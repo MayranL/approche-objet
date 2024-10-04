e5c7fa178055d80a8a966ba02ce4083a3a488ac3
Mayran <loicmayran@gmail.com>
2024-10-04 12:13:13 +0200

    TP5 : Fin

---
+++ src/main/java/fr/diginamic/banque/TestBanque.java
@@ -0,0 +1,10 @@
+package fr.diginamic.banque;
+
+import fr.diginamic.banque.entites.Compte;
+
+public class TestBanque {
+    public static void main(String[] args) {
+        Compte compte = new Compte(15,1000);
+        System.out.println(compte);
+    }
+}
